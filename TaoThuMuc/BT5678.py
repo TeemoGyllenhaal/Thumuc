124466a036d7c858ca93538e07ab55a807646bd1
Lê Quý Minh Quang <kulin2345@gmail.com>
2022-12-09 15:59:54 +0700

    Thu Muc

--- TaoThuMuc/BT5678.py
+++ TaoThuMuc/BT5678.py
@@ -1,26 +1,25 @@
+#câu 5
 #Liệt kê tất cả thư mục:
 import os
-def current_path(): 
-    print(os.getcwd()) 
-    print()
-print("Current working directory before") 
-current_path()
-os.chdir('D:/data')
-print("Current working directory after") 
-current_path()
-path = r'C:\Users\Teemo Gyllenhaal\Documents'
-dir_list = os.listdir(path)
-print("tất cả file trong", path,":")
-print(dir_list)
-path = r'D:\data'
-tontai= os.path.exists(path)
-print(tontai)
-path = r'D:\yamero'
-tontai= os.path.exists(path)
-print(tontai)
+def liet_ke(x):
+    path = x
+    dir_list = os.listdir(path)
+    print("tất cả file trong", path,":")
+    print(dir_list)
+liet_ke(r'C:\Users\Teemo Gyllenhaal\Documents')
+#câu 6
+#Kiểm tra tồn tại thư mục hay không
+def kiem_tra(x):
+    path = x
+    tontai= os.path.exists(path)
+    print(tontai)
+kiem_tra(r'D:\data')
+#7
 #Xóa thư mục
-directory = "nnlt2"
-parent_dir = "D:/data"
-path = os.path.join(parent_dir, directory)
-os.rmdir(path)
-print("Directory '% s' removed" % directory)
+def xoa_thu_muc(x,y):
+    directory = x
+    parent_dir = y
+    path = os.path.join(parent_dir, directory)
+    os.rmdir(path)
+    print("Thư mục '% s' đã xóa tại: " % directory)
+xoa_thu_muc(r"nnlt2",r"D:/data")
